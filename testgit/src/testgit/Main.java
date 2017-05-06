8134e11b4c3ec97dc8d359a20383037f287a5c66
greenstar2017 <dell@dell-PC07>
2017-05-06 17:23:09 +0800

    3

--- testgit/src/testgit/Main.java
+++ testgit/src/testgit/Main.java
@@ -3,11 +3,8 @@ package testgit;
 public class Main {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("s12345");
-=======
 		System.out.println("s1234");
->>>>>>> branch 'master' of https://github.com/greenstar2017/greenstar.git
 		System.out.println("s123457");
 		
 	}
