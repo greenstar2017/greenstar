b6d529fcd9b818b1744c9e4c0bbb1717cd0bbc17
greenstar2017 <dell@dell-PC07>
2017-05-06 16:35:27 +0800

    测试提交

--- testgit/src/testgit/Main.java
+++ testgit/src/testgit/Main.java
@@ -3,6 +3,11 @@ package testgit;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println();
+		System.out.println("s");
+		
+	}
+	
+	public String toString() {
+		return "a";
 	}
 }
