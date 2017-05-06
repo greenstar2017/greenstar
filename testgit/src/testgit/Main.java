4018310e625f7ae7dcbc3e04e51d073450e0ca08
greenstar2017 <dell@dell-PC07>
2017-05-06 17:45:36 +0800

    3

--- testgit/src/testgit/Main.java
+++ testgit/src/testgit/Main.java
@@ -5,8 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("s12345");
 		System.out.println("s123456");
+		System.out.println("s1234eee56");
 		System.out.println("s123456222222");
-		System.out.println("s123456789");
+		System.out.println("s1234567");
 		
 	}
 	
