888584d502d48e499a447925c65908e92584f4c7
greenstar2017 <dell@dell-PC07>
2017-05-06 17:39:13 +0800

    3

--- testgit/src/testgit/Main.java
+++ testgit/src/testgit/Main.java
@@ -3,6 +3,7 @@ package testgit;
 public class Main {
 	
 	public static void main(String[] args) {
+		System.out.println("s12345");
 		System.out.println("s123456");
 		System.out.println("s1234eee56");
 		System.out.println("s1234567");
